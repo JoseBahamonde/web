ff259e22499bff9363d5537ac5c960f1da670c9b
Jose Bahamonde Cordeiro <jbahamondecordeiro@gmail.com>
2016-09-06 17:42:32 +0200

    Added minify gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,34 @@
 var gulp = require('gulp');
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
 
-// Copy vendor libraries from /node_modules into /vendor
+var banner = ['/*!\n',
+    ' * Jose Bahamonde - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+    ' * Copyright 2016-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
+    ' */\n',
+    ''
+].join('');
+
+gulp.task('minify-css', function() {
+    return gulp.src('styles/common.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('styles'))
+});
+
+gulp.task('minify-js', function() {
+    return gulp.src('scripts/page-utils.js')
+        .pipe(uglify())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('scripts'))
+});
+
 gulp.task('copy', function() {
 	gulp.src(
 			[ 'node_modules/bootstrap/dist/**/*', '!**/npm.js',
@@ -26,4 +54,4 @@ gulp.task('copy', function() {
 })
 
 // Run everything
-gulp.task('default', [ 'copy' ]);
+gulp.task('default', [ 'minify-css', 'minify-js', 'copy' ]);
