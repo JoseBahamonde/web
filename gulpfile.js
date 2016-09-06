38f17dcf7440610c1daec3ada88c33a0945fcade
Jose Bahamonde Cordeiro <jbahamondecordeiro@gmail.com>
2016-09-06 19:16:39 +0200

    Added package task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
 var header = require('gulp-header');
-var clean = require('gulp-clean');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var zip = require('gulp-zip');
+var del = require('del');
 var pkg = require('./package.json');
 
 var banner = ['/*!\n',
@@ -15,35 +15,39 @@ var banner = ['/*!\n',
     ''
 ].join('');
 
+gulp.task('clean', function() {
+	del([ 'dist/**' ]);
+});
+
 gulp.task('minify-css', function() {
-    return gulp.src('styles/common.css')
+    return gulp.src('src/styles/*.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('styles'))
+        .pipe(gulp.dest('dist/page/styles'))
 });
 
 gulp.task('minify-js', function() {
-    return gulp.src('scripts/page-utils.js')
+    return gulp.src('src/scripts/*.js')
         .pipe(uglify())
         .pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('scripts'))
+        .pipe(gulp.dest('dist/page/scripts'))
 });
 
 gulp.task('copy', function() {
 	gulp.src(
 			[ 'node_modules/bootstrap/dist/**/*', '!**/npm.js',
 					'!**/bootstrap-theme.*', '!**/*.map' ]).pipe(
-			gulp.dest('vendor/bootstrap'));
+			gulp.dest('dist/page/vendor/bootstrap'));
 
 	gulp.src(
 			[ 'node_modules/jquery/dist/jquery.js',
 					'node_modules/jquery/dist/jquery.min.js' ]).pipe(
-			gulp.dest('vendor/jquery'));
+			gulp.dest('dist/page/vendor/jquery'));
 
 	gulp.src([ 'node_modules/scrollreveal/dist/*.js' ]).pipe(
-			gulp.dest('vendor/scrollreveal'));
+			gulp.dest('dist/page/vendor/scrollreveal'));
 
 	gulp.src(
 			[ 'node_modules/font-awesome/**',
@@ -52,16 +56,17 @@ gulp.task('copy', function() {
 					'!node_modules/font-awesome/*.txt',
 					'!node_modules/font-awesome/*.md',
 					'!node_modules/font-awesome/*.json' ]).pipe(
-			gulp.dest('vendor/font-awesome'));
+			gulp.dest('dist/page/vendor/font-awesome'));
 })
 
 gulp.task('default', [ 'minify-css', 'minify-js', 'copy' ]);
 
-//gulp.task('package', ['default'], function() {
-//	try {
-//		gulp.src('./dist', {read: false, force: true}).pipe(clean());
-//	} catch (e) { }
-//	
-//	gulp.src(['./**', '!**/.gitignore', '!./node_modules/'])
-//		.pipe(zip('page.zip')).pipe(gulp.dest('dist'));
-//});
+gulp.task('copy-dist', ['default'], function() {
+	gulp.src([ 'src/images/**' ]).pipe(gulp.dest('dist/page/images'));
+	gulp.src([ 'src/index.html' ]).pipe(gulp.dest('dist/page'));
+	gulp.src([ 'src/favicon.ico' ]).pipe(gulp.dest('dist/page'));
+});
+
+gulp.task('package', ['copy-dist'], function() {
+	gulp.src(['./dist/page/**']).pipe(zip('page.zip')).pipe(gulp.dest('dist'));
+});
