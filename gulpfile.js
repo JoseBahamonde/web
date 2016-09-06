71583b0a874bfc51913fb939ce34500742ffd5b7
Jose Bahamonde Cordeiro <jbahamondecordeiro@gmail.com>
2016-09-06 18:43:31 +0200

    Added src folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var header = require('gulp-header');
+var clean = require('gulp-clean');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
+var zip = require('gulp-zip');
 var pkg = require('./package.json');
 
 var banner = ['/*!\n',
@@ -33,15 +35,15 @@ gulp.task('copy', function() {
 	gulp.src(
 			[ 'node_modules/bootstrap/dist/**/*', '!**/npm.js',
 					'!**/bootstrap-theme.*', '!**/*.map' ]).pipe(
-			gulp.dest('vendor/bootstrap'))
+			gulp.dest('vendor/bootstrap'));
 
 	gulp.src(
 			[ 'node_modules/jquery/dist/jquery.js',
 					'node_modules/jquery/dist/jquery.min.js' ]).pipe(
-			gulp.dest('vendor/jquery'))
+			gulp.dest('vendor/jquery'));
 
 	gulp.src([ 'node_modules/scrollreveal/dist/*.js' ]).pipe(
-			gulp.dest('vendor/scrollreveal'))
+			gulp.dest('vendor/scrollreveal'));
 
 	gulp.src(
 			[ 'node_modules/font-awesome/**',
@@ -50,8 +52,16 @@ gulp.task('copy', function() {
 					'!node_modules/font-awesome/*.txt',
 					'!node_modules/font-awesome/*.md',
 					'!node_modules/font-awesome/*.json' ]).pipe(
-			gulp.dest('vendor/font-awesome'))
+			gulp.dest('vendor/font-awesome'));
 })
 
-// Run everything
 gulp.task('default', [ 'minify-css', 'minify-js', 'copy' ]);
+
+//gulp.task('package', ['default'], function() {
+//	try {
+//		gulp.src('./dist', {read: false, force: true}).pipe(clean());
+//	} catch (e) { }
+//	
+//	gulp.src(['./**', '!**/.gitignore', '!./node_modules/'])
+//		.pipe(zip('page.zip')).pipe(gulp.dest('dist'));
+//});
